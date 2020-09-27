77963e3b5f4d9e47be4ebad9fe2dba48d2c40dda
Reena-bains <reenabains.zapbuild@gmail.com>
2020-09-27 13:03:43 +0530

    merge code

--- app/src/main/java/com/example/firebasefacebooklogin/MainActivity.kt
+++ app/src/main/java/com/example/firebasefacebooklogin/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var callbackManager: CallbackManager
     private lateinit var auth: FirebaseAuth
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
